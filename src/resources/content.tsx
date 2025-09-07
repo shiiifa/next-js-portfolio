a9640c940769d64c1c060f4b6081c2191d7b9808
shiiiifa <shishigabbey@gmail.com>
2025-09-07 12:37:54 +0000

    Fix Projects

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -234,8 +234,8 @@ const blog: Blog = {
 
 const work: Work = {
   path: "/work",
-  label: "Projects",
-  title: `Projects – ${person.name}`,
+  label: "Work",
+  title: `Work – ${person.name}`,
   description: `Design and dev projects by ${person.name}`,
   // Create new project pages by adding a new .mdx file to app/blog/posts
   // All projects will be listed on the /home and /work routes
