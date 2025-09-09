edbd1b9179ceb31550486b48e19e6a883ec1e0a2
shiiiifa <shishigabbey@gmail.com>
2025-09-09 15:13:33 +0000

    fixed references

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -67,7 +67,7 @@ const about: About = {
   path: "/about",
   label: "About",
   title: `About – ${person.name}`,
-  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+  description: `Meet ${person.name}, ${person.role}`,
   tableOfContent: {
     display: true,
     subItems: false,
