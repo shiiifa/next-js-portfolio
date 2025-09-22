695be7ccddc1e5bef64037a11e88e1124736d740
shiiiifa <shishigabbey@gmail.com>
2025-09-22 12:46:37 +0000

    l

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -78,7 +78,7 @@ export default function About() {
             position="sticky"
             top="64"
             s={{ position: "relative" }}
-            minWidth="200"
+            minWidth="192"
             paddingX="l"
             paddingBottom="xl"
             gap="m"
