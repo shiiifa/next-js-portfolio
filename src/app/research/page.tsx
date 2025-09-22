f6d379fed9ed77d19182b58da0f1427e2b522015
shiiiifa <shishigabbey@gmail.com>
2025-09-22 00:23:47 +0000

    header fix

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -14,7 +14,7 @@ export async function generateMetadata() {
 
 export default function Research() {
   return (
-    <Column maxWidth="m" paddingTop="24" style={{ animation: "fadeIn 1s ease-out" }}>
+    <Column maxWidth="m" paddingTop="24" className="fixed-header-spacing" style={{ animation: "fadeIn 1s ease-out" }}>
       <style dangerouslySetInnerHTML={{
         __html: `
           @keyframes fadeInPop {
